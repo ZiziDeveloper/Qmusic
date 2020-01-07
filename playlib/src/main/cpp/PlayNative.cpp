0bb063528986653da3510116d2fc526dd4479164
qiuyayong <qiuyayong@lizhi.fm>
2020-01-07 17:02:01 +0800

    [update]:jni调用改为动态注册

--- playlib/src/main/cpp/PlayNative.cpp
+++ playlib/src/main/cpp/PlayNative.cpp
@@ -23,6 +23,189 @@ void nativeStop() {
 /***********************************************************************
  * jni method
  */
+
+
+extern "C" {
+
+    /**
+     * 播放准备
+     * @param env
+     * @param instance
+     * @param source_
+     * @param volume
+     * @param layout
+     */
+    void nativePrepare(JNIEnv *env, jobject instance,
+                        jstring source_, jint volume, jint layout) {
+        const char *source = env->GetStringUTFChars(source_, 0);
+        int length = env->GetStringLength(source_);
+        PlaySession::getIns()->allocUrl((char *) source, length);
+        NotifyApplication::getIns()->init(javaVM, env, &instance);
+        nativeStop();
+        if (nullptr == pAudioProccessor) {
+            pAudioProccessor = new AudioProccessor;
+        }
+        pAudioProccessor->prepare();
+        LOGI("native_prepare url : %s ", PlaySession::getIns()->getUrl());
+        env->ReleaseStringUTFChars(source_, source);
+    }
+
+    /**
+     * 开始播放
+     * @param env
+     * @param instance
+     */
+    void nativeStart(JNIEnv *env, jobject instance) {
+        if (nullptr != pAudioProccessor) {
+            pAudioProccessor->start();
+        }
+    }
+
+    /**
+     * 继续播放
+     * @param env
+     * @param instance
+     */
+    void nativeResume(JNIEnv *env, jobject instance) {
+        if (nullptr != pAudioProccessor) {
+            pAudioProccessor->resume();
+            NotifyApplication::getIns()->notifyResumed(MAIN_THREAD);
+        }
+    }
+
+    /**
+     * 暂停播放
+     * @param env
+     * @param instance
+     */
+    void nativePause(JNIEnv *env, jobject instance) {
+        if (nullptr != pAudioProccessor) {
+            pAudioProccessor->pause();
+            NotifyApplication::getIns()->notifyPaused(MAIN_THREAD);
+        }
+    }
+
+    /**
+     * 停止播放
+     * @param env
+     * @param instance
+     */
+    void nativeJniStop(JNIEnv *env, jobject instance) {
+        nativeStop();
+    }
+
+    /**
+     * 拖动播放进度
+     * @param env
+     * @param instance
+     * @param progress
+     */
+    void nativeSeek(JNIEnv *env, jobject instance, jint progress) {
+        if (nullptr != pAudioProccessor) {
+            pAudioProccessor->seek(progress);
+            NotifyApplication::getIns()->notifySeeked(MAIN_THREAD, progress);
+        }
+    }
+
+    /**
+     * 音频时长
+     * @param env
+     * @param instance
+     * @return
+     */
+    jint nativeDuration(JNIEnv *env, jobject instance) {
+        // TODO
+        return 0;
+    }
+
+    /**
+     * 设置声音
+     * @param env
+     * @param instance
+     * @param percent
+     */
+    void nativeVolume(JNIEnv *env, jobject instance, jint percent) {
+        if (nullptr != pAudioProccessor) {
+            pAudioProccessor->setVolume(percent);
+            NotifyApplication::getIns()->notifyVolumeModified(MAIN_THREAD, percent);
+        }
+    }
+
+    /**
+     * 改变声道
+     * @param env
+     * @param instance
+     * @param channel
+     */
+    void nativeChannelSwitch(JNIEnv *env, jobject instance,jint channel) {
+        if (nullptr != pAudioProccessor) {
+            pAudioProccessor->switchChannel(channel);
+            NotifyApplication::getIns()->notifyChannelLayoutModified(MAIN_THREAD, channel);
+        }
+    }
+
+    /**
+     * 改变音调
+     * @param env
+     * @param instance
+     * @param pitch
+     */
+    void nativePitch(JNIEnv *env, jobject instance, jfloat pitch) {
+        if (nullptr != pAudioProccessor) {
+            pAudioProccessor->setPitch(pitch);
+            NotifyApplication::getIns()->notifyPitchModified(MAIN_THREAD, pitch);
+        }
+    }
+
+    /**
+     * 改变播放速度
+     * @param env
+     * @param instance
+     * @param speed
+     */
+    void nativeSpeed(JNIEnv *env, jobject instance, jfloat speed) {
+        if (nullptr != pAudioProccessor) {
+            pAudioProccessor->setSpeed(speed);
+            NotifyApplication::getIns()->notifySpeedModified(MAIN_THREAD, speed);
+        }
+    }
+
+    /**
+     * 获取采样率
+     * @param env
+     * @param instance
+     * @return
+     */
+    jint nativeSamplerate(JNIEnv *env, jobject instance) {
+        // TODO
+        return 0;
+    }
+
+    jint RegisterNativeMethods(JNIEnv *env) {
+        jclass clazz = env->FindClass("com/zizi/playlib/PlayJniProxy");
+        if (clazz == nullptr) {
+            LOGE("con't find class: com/zizi/playlib/PlayJniProxy");
+            return JNI_ERR;
+        }
+        JNINativeMethod methods_Proxy[] = {
+                {"native_prepare", "(Ljava/lang/String;II)V", (void *) nativePrepare},
+                {"native_start", "()V", (void *) nativeStart},
+                {"native_resume", "()V", (void *) nativeResume},
+                {"native_pause", "()V", (void *) nativePause},
+                {"native_stop", "()V", (void *) nativeJniStop},
+                {"native_seek", "(I)V", (void *) nativeSeek},
+                {"native_duration", "()I", (void *) nativeDuration},
+                {"native_volume", "(I)V", (void *) nativeVolume},
+                {"native_channel_switch", "(I)V", (void *) nativeChannelSwitch},
+                {"native_pitch", "(F)V", (void *) nativePitch},
+                {"native_speed", "(F)V", (void *) nativeSpeed},
+                {"native_samplerate", "()I", (void *) nativeSamplerate}
+
+        };
+        return env->RegisterNatives(clazz, methods_Proxy, sizeof(methods_Proxy) / sizeof(methods_Proxy[0]));
+    }
+}
+
 extern "C"
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved)
 {
@@ -34,117 +217,8 @@ JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved)
 
         return result;
     }
+    result = RegisterNativeMethods(env);
+    LOGI("RegisterNativeMethods result : %d ", result);
     return JNI_VERSION_1_4;
-
 }
 
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_zizi_playlib_PlayJniProxy_native_1prepare(JNIEnv *env, jobject instance,
-                                                      jstring source_, jint volume, jint layout) {
-    const char *source = env->GetStringUTFChars(source_, 0);
-    int length = env->GetStringLength(source_);
-    PlaySession::getIns()->allocUrl((char *) source, length);
-    NotifyApplication::getIns()->init(javaVM, env, &instance);
-    nativeStop();
-    if (nullptr == pAudioProccessor) {
-        pAudioProccessor = new AudioProccessor;
-    }
-    pAudioProccessor->prepare();
-    LOGI("native_prepare url : %s ", PlaySession::getIns()->getUrl());
-    env->ReleaseStringUTFChars(source_, source);
-}
-
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_zizi_playlib_PlayJniProxy_native_1start(JNIEnv *env, jobject instance) {
-    if (nullptr != pAudioProccessor) {
-        pAudioProccessor->start();
-    }
-}
-
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_zizi_playlib_PlayJniProxy_native_1resume(JNIEnv *env, jobject instance) {
-    if (nullptr != pAudioProccessor) {
-        pAudioProccessor->resume();
-        NotifyApplication::getIns()->notifyResumed(MAIN_THREAD);
-    }
-}
-
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_zizi_playlib_PlayJniProxy_native_1pause(JNIEnv *env, jobject instance) {
-    if (nullptr != pAudioProccessor) {
-        pAudioProccessor->pause();
-        NotifyApplication::getIns()->notifyPaused(MAIN_THREAD);
-    }
-}
-
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_zizi_playlib_PlayJniProxy_native_1stop(JNIEnv *env, jobject instance) {
-    nativeStop();
-}
-
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_zizi_playlib_PlayJniProxy_native_1seek(JNIEnv *env, jobject instance, jint progress) {
-    if (nullptr != pAudioProccessor) {
-        pAudioProccessor->seek(progress);
-        NotifyApplication::getIns()->notifySeeked(MAIN_THREAD, progress);
-    }
-}
-
-extern "C"
-JNIEXPORT jint JNICALL
-Java_com_zizi_playlib_PlayJniProxy_native_1duration(JNIEnv *env, jobject instance) {
-
-    // TODO
-
-}
-
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_zizi_playlib_PlayJniProxy_native_1volume(JNIEnv *env, jobject instance, jint percent) {
-    if (nullptr != pAudioProccessor) {
-        pAudioProccessor->setVolume(percent);
-        NotifyApplication::getIns()->notifyVolumeModified(MAIN_THREAD, percent);
-    }
-}
-
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_zizi_playlib_PlayJniProxy_native_1pitch(JNIEnv *env, jobject instance, jfloat pitch) {
-    if (nullptr != pAudioProccessor) {
-        pAudioProccessor->setPitch(pitch);
-        NotifyApplication::getIns()->notifyPitchModified(MAIN_THREAD, pitch);
-    }
-}
-
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_zizi_playlib_PlayJniProxy_native_1speed(JNIEnv *env, jobject instance, jfloat speed) {
-    if (nullptr != pAudioProccessor) {
-        pAudioProccessor->setSpeed(speed);
-        NotifyApplication::getIns()->notifySpeedModified(MAIN_THREAD, speed);
-    }
-}
-
-extern "C"
-JNIEXPORT jint JNICALL
-Java_com_zizi_playlib_PlayJniProxy_native_1samplerate(JNIEnv *env, jobject instance) {
-
-    // TODO
-
-}
-
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_zizi_playlib_PlayJniProxy_native_1channel_1switch(JNIEnv *env, jobject instance,
-                                                             jint channel) {
-    if (nullptr != pAudioProccessor) {
-        pAudioProccessor->switchChannel(channel);
-        NotifyApplication::getIns()->notifyChannelLayoutModified(MAIN_THREAD, channel);
-    }
-}
